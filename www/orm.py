1caae94052c1885ebdf2030a04752f61bdc3f1e6
xieyifan <xieyifan@wesai.com>
2017-08-25 17:30:33 +0800

    first commit

---
+++ www/orm.py
@@ -0,0 +1,38 @@
+# coding:utf8
+import asyncio, logging, aiomysql
+
+
+# 链接数据库
+@asyncio.coroutine
+def create_pool(loop, **kw):
+    logging.info('create database connection pool...')
+    global __pool
+    __pool = yield from aiomysql.create_pool(
+        host=kw.get('host', 'localhost'),
+        port=kw.get('port', 3306),
+        user=kw['woniu'],
+        password=kw['123456'],
+        db=kw['web'],
+        charset=kw.get('charset', 'utf8'),
+        autocommit=kw.get('autocommit', True),
+        maxsize=kw.get('maxsize', 10),
+        minsize=kw.get('minsize', 1),
+        loop=loop
+    )
+
+
+# 定义select
+@asyncio.coroutine
+def select(sql, args, size=None):   # size 为默认返回的条目数量， 默认为None；
+    log(sql, args)
+    global __pool
+    with (yield from __pool) as conn:
+        cur = yield from conn.cursor(aiomysql.DictCursor)
+        yield from cur.execute(sql.replace('?', '%s'), args or ())
+        if size:
+            rs = yield from cur.fetchmany(size)
+        else:
+            rs = yield from cur.fetchall()
+        yield from cur.close()
+        logging.info('rows returned: %s' % len(rs))
+        return rs
